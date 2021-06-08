1b342da4766bbcdabe3781dcbf63202d325afef9
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-07 19:41:57 -0500

    Chatty Bot Stage 3

--- Chatty Bot/chattybot.py
+++ Chatty Bot/chattybot.py
@@ -5,3 +5,13 @@ print("Please, remind me your name.")
 # reading a name
 name = input()
 print("What a great name you have, " + name + "!")
+
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
