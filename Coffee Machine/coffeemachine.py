35c3e08f46042aba8563e5cdb050b80850d465d2
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-09 22:33:19 -0500

    Coffee Machine Stage 3

--- Coffee Machine/coffeemachine.py
+++ Coffee Machine/coffeemachine.py
@@ -1,15 +1,22 @@
-# Write your code here
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-print("Pouring some milk into the cup")
-print("Coffee is ready!")
-
-print("Write how many cups of coffee you will need: ")
-cups = int(input())
-print("For " + str(cups) + " cups of coffee you will need:")
-print(str((cups * 200)) + " ml of water")
-print(str((cups * 50)) + " ml of milk")
-print(str((cups * 15)) + " g of coffee beans")
+print("Write how many ml of water the coffee machine has:")
+water = int(input())
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input())
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee_beans = int(input())
+print("Write how many cups of coffee you will need:")
+requested = int(input())
+possible = 0
+while (water >= 200 and milk >= 50 and coffee_beans >= 15):
+    possible += 1
+    water -= 200
+    milk -= 50
+    coffee_beans -= 15
+if possible < requested:
+    print("No, I can make only " + str(possible) + " cups of coffee")
+else:
+    print("Yes, I can make that amount of coffee", end = "")
+    if possible > requested:
+        extra = str(possible - requested)
+        print("(and even " + extra + " more than that)", end = "")
+    print()
