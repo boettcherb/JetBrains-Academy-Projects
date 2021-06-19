0ea4b6ed77bec9a6d6187a3012eacb9fb7955799
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-18 23:37:00 -0500

    Loan Calculator Stage 2

--- Loan Calculator/loancalculator.py
+++ Loan Calculator/loancalculator.py
@@ -1,12 +1,20 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-# write your code here
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+loan_principal = int(input("Enter the loan principal:\n"))
+print("What do you want to calculate?")
+print('type "m" for number of monthly payments,')
+print('type "p" for the monthly payment')
+
+if input() == "m":
+    payment = int(input("Enter the monthly payment:\n"))
+    months = math.ceil(loan_principal / payment)
+    s = "s" if months > 1 else ""
+    print(f"\nIt will take {months} month{s} to repay the loan")
+else:
+    months = int(input("Enter the number of months:\n"))
+    payment = math.ceil(loan_principal / months)
+    print(f"\nYour monthly payment = {payment}", end="")
+    if loan_principal % payment != 0:
+        last_payment = loan_principal - payment * (months - 1)
+        print(f" and the last payment = {last_payment}.", end="")
+    print()
