889f5dec6e08d64ddb03854745317d60d3e94104
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-19 00:18:40 -0500

    Loan Calculator Stage 3

--- Loan Calculator/loancalculator.py
+++ Loan Calculator/loancalculator.py
@@ -1,20 +1,27 @@
 import math
 
-loan_principal = int(input("Enter the loan principal:\n"))
 print("What do you want to calculate?")
-print('type "m" for number of monthly payments,')
-print('type "p" for the monthly payment')
-
-if input() == "m":
-    payment = int(input("Enter the monthly payment:\n"))
-    months = math.ceil(loan_principal / payment)
-    s = "s" if months > 1 else ""
-    print(f"\nIt will take {months} month{s} to repay the loan")
-else:
-    months = int(input("Enter the number of months:\n"))
-    payment = math.ceil(loan_principal / months)
-    print(f"\nYour monthly payment = {payment}", end="")
-    if loan_principal % payment != 0:
-        last_payment = loan_principal - payment * (months - 1)
-        print(f" and the last payment = {last_payment}.", end="")
-    print()
+print('type "n" for number of monthly payments,')
+print('type "a" for annuity monthly payment amount,')
+print('type "p" for loan principal:')
+letter = input()
+if letter == "n":
+    p = float(input("Enter the loan principal:\n"))
+    a = float(input("Enter the monthly payment:\n"))
+    i = float(input("Enter the loan interest:\n")) / 1200
+    n = math.ceil(math.log(a / (a - i * p), 1 + i))
+    years = n // 12
+    time = ("" if years == 0 else f"{years} years and ") + f"{n % 12} months"
+    print(f"It will take {time} to repay this loan!")
+elif letter == "a":
+    p = float(input("Enter the loan principal:\n"))
+    n = float(input("Enter the number of periods:\n"))
+    i = float(input("Enter the loan interest:\n")) / 1200
+    a = p * (i * (1 + i) ** n) / ((1 + i) ** n - 1)
+    print(f"Your monthly payment = {math.ceil(a)}!")
+elif letter == "p":
+    a = float(input("Enter the monthly payment:\n"))
+    n = float(input("Enter the number of periods:\n"))
+    i = float(input("Enter the loan interest:\n")) / 1200
+    p = a / ((i * (1 + i) ** n) / ((1 + i) ** n - 1))
+    print(f"Your loan principal = {round(p)}!")
