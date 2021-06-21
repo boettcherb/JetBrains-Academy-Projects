ca1fc74adda94d92982c987170d57264328d1e7f
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-21 14:17:35 -0500

    Bill Splitter Stage 4

--- Bill Splitter/billsplitter.py
+++ Bill Splitter/billsplitter.py
@@ -6,12 +6,15 @@ if n > 0:
     print("Enter the name of every friend (including you), each on a new line:")
     names = [input() for _ in range(n)]
     bill = int(input("\nEnter the total bill value:\n"))
-    share = round(bill / n, 2)
-    friends = dict.fromkeys(names, share)
     print('\nDo you want to use the "Who is lucky?" feature? Write Yes / No:')
     if input() == "Yes":
-        print(f"\n{random.choice(names)} is the lucky one!")
+        lucky_person = random.choice(names)
+        print(f"\n{lucky_person} is the lucky one!\n")
+        friends = dict.fromkeys(names, round(bill / (n - 1), 2))
+        friends[lucky_person] = 0
+        print(friends)
     else:
-        print("\nNo one is going to be lucky")
+        print("\nNo one is going to be lucky\n")
+        print(dict.fromkeys(names, round(bill / n, 2)))
 else:
     print("\nNo one is joining for the party")
