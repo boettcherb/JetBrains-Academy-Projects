19dcc8d51227e08f510e8c7e8e0157bdf3b9e851
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-21 13:55:55 -0500

    Bill Splitter Stage 2

--- Bill Splitter/billsplitter.py
+++ Bill Splitter/billsplitter.py
@@ -2,7 +2,9 @@ n = int(input("Enter the number of friends joining (including you):\n"))
 if n > 0:
     print()
     print("Enter the name of every friend (including you), each on a new line:")
-    friends = dict.fromkeys([input() for _ in range(0, n)], 0)
-    print("\n" + str(friends))
+    names = [input() for _ in range(n)]
+    bill = int(input("\nEnter the total bill value:\n"))
+    share = round(bill / n, 2)
+    print("\n" + str(dict.fromkeys(names, share)))
 else:
     print("\nNo one is joining for the party")
