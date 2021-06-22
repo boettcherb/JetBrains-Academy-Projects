38669f47bff6672abecb4e54c1369c64a70003b5
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-22 14:35:07 -0500

    Generating Randomness Stage 2

--- Generating Randomness/generatingrandomness.py
+++ Generating Randomness/generatingrandomness.py
@@ -7,4 +7,14 @@ while True:
         print(f"Current data length is {length}, {100 - length} symbols left")
     else:
         break
-print("\nFinal data string:\n" + "".join(binary_digits))
+binary_string = "".join(binary_digits)
+print(f"\nFinal data string:\n{binary_string}\n")
+
+counts = {"000": [0, 0], "001": [0, 0], "010": [0, 0], "011": [0, 0],
+          "100": [0, 0], "101": [0, 0], "110": [0, 0], "111": [0, 0]}
+for i in range(len(binary_string) - 3):
+    digit = int(binary_string[i + 3])
+    counts[binary_string[i:i + 3]][digit] += 1
+
+for key in counts:
+    print(f"{key}: {counts[key][0]},{counts[key][1]}")
