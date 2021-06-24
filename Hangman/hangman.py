cfa62683c30d4c63700e4bd7f071a87fb7fc0649
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-23 19:07:16 -0500

    Hangman Stage 3

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,5 +1,8 @@
+import random
+
 print("H A N G M A N")
-if input("Guess the word: ") == "python":
+words = ("python", "java", "kotlin", "javascript")
+if input("Guess the word: ") == random.choice(words):
     print("You survived!")
 else:
     print("You lost!")
