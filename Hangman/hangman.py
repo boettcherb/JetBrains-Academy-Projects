c9c526f1e0089f760e47fe4d57ab8690c3077399
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-24 01:55:44 -0500

    Hangman Stage 7

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -11,21 +11,29 @@ def main():
     word = random.choice(("python", "java", "kotlin", "javascript"))
     display = list("-" * len(word))
     letters_to_guess = set(word)
+    guessed_letters = set()
     lives = 8
     while lives > 0:
         print("\n" + "".join(display))
         if len(letters_to_guess) == 0:
-            print("You guessed the word!")
+            print(f"You guessed the word {word}!")
             break
         letter = input("Input a letter: ")
-        if letter not in letters_to_guess:
+        if len(letter) != 1:
+            print("You should input a single letter")
+        elif not letter.islower():
+            print("Please enter a lowercase English letter")
+        elif letter in guessed_letters:
+            print("You've already guessed this letter")
+        elif letter not in letters_to_guess:
             lives -= 1
-            print("No improvements" if letter in word else
-                  "That letter doesn't appear in the word")
+            print("That letter doesn't appear in the word")
+            guessed_letters.add(letter)
         else:
             letters_to_guess.remove(letter)
             for index in find_letter(word, letter):
                 display[index] = letter
+            guessed_letters.add(letter)
     print("You lost!" if lives == 0 else "You survived!")
 
 
