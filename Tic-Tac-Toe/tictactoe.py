2ef4f2b7140a2a090755fbe5e40fea752d2ed4a4
Brandon Boettcher <brandonb@cs.utexas.edu>
2021-06-25 20:06:37 -0500

    Tic-Tac-Toe Stage 5

--- Tic-Tac-Toe/tictactoe.py
+++ Tic-Tac-Toe/tictactoe.py
@@ -1,8 +1,8 @@
 class Board:
-    def __init__(self, cells):
+    def __init__(self, cells="_________"):
         self.count_x = cells.count("X")
         self.count_o = cells.count("O")
-        self.board = list(cells)
+        self.board = [" " if c == "_" else c for c in cells]
 
     def __str__(self):
         r1 = " ".join(self.board[0:3])
@@ -15,22 +15,16 @@ class Board:
         if i not in (1, 2, 3) or j not in (1, 2, 3):
             return False, "Coordinates should be from 1 to 3!"
         index = (j - 1) + 3 * (i - 1)
-        if self.board[index] == "_":
+        if self.board[index] == " ":
             self.board[index] = player
             self.count_x += player == "X"
             self.count_o += player == "O"
             return True, ""
         return False, "This cell is occupied! Choose another one!"
 
-    def valid_board(self):
-        """Return False if the current game state is not possible"""
-        if abs(self.count_x - self.count_o) > 1:
-            return False
-        return not self.is_winner("X") or not self.is_winner("O")
-
     def is_winner(self, player):
         """Return true if player (either "X" or "O") has 3 in a row"""
-        for i in range(3):
+        for i in range(0, 9, 3):
             if self.board[i:i + 3].count(player) == 3:
                 return True
         for i in range(3):
@@ -38,20 +32,17 @@ class Board:
                 return True
         if self.board[::4].count(player) == 3:
             return True
-        return self.board[2::2].count(player) == 3
+        return self.board[2:7:2].count(player) == 3
 
     def evaluate_position(self):
-        """Print a message describing the current state of the board"""
-        if not self.valid_board():
-            print("Impossible")
-        elif self.is_winner("X"):
-            print("X wins")
+        """Return a message describing the current state of the board"""
+        if self.is_winner("X"):
+            return "X wins"
         elif self.is_winner("O"):
-            print("O wins")
+            return "O wins"
         elif self.count_o + self.count_x == 9:
-            print("Draw")
-        else:
-            print("Game not finished")
+            return "Draw"
+        return "Game not finished"
 
 
 def get_coordinates():
@@ -64,15 +55,21 @@ def get_coordinates():
 
 
 def main():
-    board = Board(input("Enter cells: "))
+    board = Board()
+    board_state = board.evaluate_position()
+    player = "X"
+    while board_state == "Game not finished":
+        print(board)
+        while True:
+            i, j = get_coordinates()
+            valid_move, message = board.make_move(i, j, player)
+            if valid_move:
+                break
+            print(message)
+        player = "X" if player == "O" else "O"
+        board_state = board.evaluate_position()
     print(board)
-    while True:
-        i, j = get_coordinates()
-        valid_move, message = board.make_move(i, j, "X")
-        if valid_move:
-            break
-        print(message)
-    print(board)
+    print(board_state)
 
 
 if __name__ == "__main__":
