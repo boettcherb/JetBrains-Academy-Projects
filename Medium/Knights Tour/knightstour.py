9b27a13db0057b087724f7c391dce6fb3767dd8e
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-06-26 12:21:54 -0500

    Knights Tour Stage 2

--- Medium/Knights Tour/knightstour.py
+++ Medium/Knights Tour/knightstour.py
@@ -1,39 +1,54 @@
-def get_coordinates():
-    """Get the knight's starting position from the user"""
-    while True:
-        try:
-            x, y = input("Enter the knight's starting position: ").split()
-            x = int(x)
-            y = int(y)
-            if 1 <= x <= 8 and 1 <= y <= 8:
-                return 8 - y, x - 1
-        except ValueError:
-            print("Invalid dimensions!")
-        else:
-            print("Invalid dimensions!")
+class Board:
+    def __init__(self):
+        self.board = []
+        while True:
+            try:
+                x, y = input("Enter your board dimensions: ").split()
+                self.rows = int(y)
+                self.cols = int(x)
+                if self.rows > 0 and self.cols > 0:
+                    break
+                print("Invalid dimensions!")
+            except ValueError:
+                print("Invalid dimensions!")
+        self.square_length = len(str(self.rows * self.cols))
+        one_square = "_" * self.square_length
+        for _ in range(self.rows):
+            self.board.append([one_square for _ in range(self.cols)])
+        self.row_just = len(str(self.rows))
 
+    def print_board(self):
+        top_len = self.cols * (self.square_length + 1) + 3
+        print(" " * self.row_just + "-" * top_len)
+        for i in range(self.rows):
+            line_start = str(self.rows - i).rjust(self.row_just) + "|"
+            print(line_start, *self.board[i], "|")
+        print(" " * self.row_just + "-" * top_len)
+        print(" " * (self.row_just + 2), end="")
+        for num in range(1, self.cols + 1):
+            num_str = str(num).rjust(self.square_length)
+            print(num_str + " ", end="")
+        print()
 
-def print_board(board):
-    print(" -------------------")
-    print("8|", *board[0], "|")
-    print("7|", *board[1], "|")
-    print("6|", *board[2], "|")
-    print("5|", *board[3], "|")
-    print("4|", *board[4], "|")
-    print("3|", *board[5], "|")
-    print("2|", *board[6], "|")
-    print("1|", *board[7], "|")
-    print(" -------------------")
-    print("   1 2 3 4 5 6 7 8")
+    def set_knight(self):
+        while True:
+            try:
+                x, y = input("Enter the knight's starting position: ").split()
+                x = int(x)
+                y = int(y)
+                if 1 <= x <= self.cols and 1 <= y <= self.rows:
+                    square = ((self.square_length - 1) * " ") + "X"
+                    self.board[self.rows - y][x - 1] = square
+                    break
+                print("Invalid position!")
+            except ValueError:
+                print("Invalid position!")
 
 
 def main():
-    board = []
-    for _ in range(8):
-        board.append(list("________"))
-    x, y = get_coordinates()
-    board[x][y] = "X"
-    print_board(board)
+    board = Board()
+    board.set_knight()
+    board.print_board()
 
 
 if __name__ == "__main__":
