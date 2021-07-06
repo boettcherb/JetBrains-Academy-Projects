59b865d9cbd37033a25f18d73373ae500cf8968c
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-06 15:44:02 -0500

    Numeric Matrix Processor Stage 3

--- Medium/Numeric Matrix Processor/README.md
+++ Medium/Numeric Matrix Processor/README.md
@@ -128,3 +128,113 @@ Output:
 0 0 0
 0 0 0
 ```
+
+## 3. Matrix by Matrix Multiplication
+
+### Description
+
+The next stage is the multiplication of matrices. This operation is a little more complex because it’s not enough to simply multiply the corresponding elements.
+
+Unlike with addition, the sizes of the matrices can be different: the only restriction is that the number of columns in the first matrix should equal the number of rows for the second matrix. The multiplication of matrix A with n rows and m columns and matrix B with m rows and k columns is C = A x B. The resulting matrix has n rows and k columns, where every element is a sum of the multiplication of m elements across the rows of matrix A by m elements down the columns of matrix B.
+
+Another really important thing is that A x B is not equal to B x A​. In fact, these are not even possible to multiply if k != i. If k = i, the resulting matrices would still be different.
+
+### Objectives
+
+In this stage, you should write a program that can do all operations on matrices that you've learned.
+
+Write a program that does the following:
+
+1. Prints a menu consisting of 4 options. The example shows what the menu should look like.
+2. Reads the user's choice.
+3. Reads all data (matrices, constants) needed to perform the chosen operation. The example shows the input format in each case.
+4. Calculates the result and outputs it. The example shows how your output should look like.
+5. Repeats all these steps.
+
+The program should keep repeating this until the `"Exit"` option is chosen.
+
+If some operation cannot be performed, output a warning message.
+
+Also, you should support floating-point numbers.
+
+### Example
+
+The greater-than symbol followed by a space (`> `) represents the user input. Note that it's not part of the input.
+```
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+Your choice: > 1
+Enter size of first matrix: > 4 5
+Enter first matrix:
+> 1 2 3 4 5
+> 3 2 3 2 1
+> 8 0 9 9 1
+> 1 3 4 5 6
+Enter size of second matrix: > 4 5
+Enter second matrix:
+> 1 1 4 4 5
+> 4 4 5 7 8
+> 1 2 3 9 8
+> 1 0 0 0 1
+The result is:
+2 3 7 8 10
+7 6 8 9 9
+9 2 12 18 9
+2 3 4 5 7
+
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+Your choice: > 2
+Enter size of matrix: > 2 2
+Enter matrix:
+> 1.5 7.0
+> 6.0 5.0
+Enter constant: > 0.5
+The result is:
+0.75 3.5
+3.0 2.5
+
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+Your choice: > 3
+Enter size of first matrix: > 3 3
+Enter first matrix:
+> 1 7 7
+> 6 6 4
+> 4 2 1
+Enter size of second matrix: > 3 3
+Enter second matrix:
+> 3 2 4
+> 5 5 9
+> 8 0 10
+The result is:
+94 37 137
+80 42 118
+30 18 44
+
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+Your choice: > 1
+Enter size of first matrix: > 2 2
+Enter first matrix:
+> 1 2
+> 3 2
+Enter size of second matrix: > 1 1
+Enter second matrix:
+> 1
+The operation cannot be performed.
+
+1. Add matrices
+2. Multiply matrix by a constant
+3. Multiply matrices
+0. Exit
+Your choice: > 0
+```
--- Medium/Numeric Matrix Processor/numericmatrixprocessor.py
+++ Medium/Numeric Matrix Processor/numericmatrixprocessor.py
@@ -1,27 +1,40 @@
 class Matrix:
-    def __init__(self, rows, cols, matrix):
+    def __init__(self, rows, cols, matrix=None):
         self.rows = rows
         self.cols = cols
-        self.matrix = matrix
+        if matrix is None:
+            self.matrix = [[0 for _ in range(cols)] for _ in range(rows)]
+        else:
+            self.matrix = matrix
 
     def __getitem__(self, item):
         return self.matrix[item]
 
     def __add__(self, other):
         if self.rows != other.rows or self.cols != other.cols:
-            return "ERROR"
+            return None
         new_matrix = Matrix(self.rows, self.cols, self.matrix[:])
         for i in range(self.rows):
             for j in range(self.cols):
                 new_matrix[i][j] += other.matrix[i][j]
         return new_matrix
 
-    def __mul__(self, scalar):
-        new_matrix = Matrix(self.rows, self.cols, self.matrix[:])
-        for row in new_matrix:
-            for j in range(self.cols):
-                row[j] *= scalar
-        return new_matrix
+    def __mul__(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            new_matrix = Matrix(self.rows, self.cols, self.matrix[:])
+            for row in new_matrix:
+                for j in range(self.cols):
+                    row[j] *= other
+            return new_matrix
+        else:
+            if self.cols != other.rows:
+                return None
+            new_matrix = Matrix(self.rows, other.cols)
+            for i in range(self.rows):
+                for j in range(other.cols):
+                    for k in range(self.cols):
+                        new_matrix[i][j] += self.matrix[i][k] * other.matrix[k][j]
+            return new_matrix
 
     def __str__(self):
         string = ""
@@ -30,19 +43,44 @@ class Matrix:
         return string
 
 
-def get_matrix_from_user():
+def get_matrix(size_prompt, matrix_prompt):
     matrix = []
-    rows, cols = [int(a) for a in input().split()]
+    rows, cols = (int(a) for a in input(size_prompt).split())
+    print(matrix_prompt)
     for _ in range(rows):
-        matrix.append([int(a) for a in input().split()])
+        uip = input().split()
+        matrix.append([int(a) if a.isdecimal() else float(a) for a in uip])
     return Matrix(rows, cols, matrix)
 
 
 def main():
-    a = get_matrix_from_user()
-    scalar = int(input())
-    print(a * scalar)
-
+    while True:
+        print("1. Add matrices")
+        print("2. Multiply matrix by a constant")
+        print("3. Multiply matrices")
+        print("0. Exit")
+        choice = int(input("Your choice: "))
+        res = None
+        if choice == 0:
+            break
+        if choice == 1:
+            a = get_matrix("Enter size of first matrix: ", "Enter first matrix:")
+            b = get_matrix("Enter size of second matrix: ", "Enter second matrix:")
+            res = a + b
+        elif choice == 2:
+            a = get_matrix("Enter size of matrix: ", "Enter matrix:")
+            s = input("Enter constant: ")
+            scalar = int(s) if s.isdecimal() else float(s)
+            res = a * scalar
+        elif choice == 3:
+            a = get_matrix("Enter size of first matrix: ", "Enter first matrix:")
+            b = get_matrix("Enter size of second matrix: ", "Enter second matrix:")
+            res = a * b
+        if res == None:
+            print("The operation could not be performed")
+        else:
+            print("The result is:")
+            print(res)
 
 if __name__ == "__main__":
     main()
