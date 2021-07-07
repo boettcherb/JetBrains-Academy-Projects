de02e743d1ced30d886403cdc8d0c53ccac78c1a
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-07 14:26:52 -0500

    University Admission Procedure Stage 3

--- Medium/University Admission Procedure/universityadmissionprocedure.py
+++ Medium/University Admission Procedure/universityadmissionprocedure.py
@@ -1,9 +1,11 @@
-a = int(input())
-b = int(input())
-c = int(input())
-average = (a + b + c) / 3
-print(average)
-if average >= 60.0:
-    print("Congratulations, you are accepted!")
-else:
-    print("We regret to inform you that we will not be able to offer you admission.")
+num_applicants = int(input())
+num_to_accept = int(input())
+applicants = []
+for _ in range(num_applicants):
+    first, last, gpa = input().split()
+    applicants.append((first + " " + last, float(gpa)))
+applicants.sort(key=(lambda a: a[0]))
+applicants.sort(key=(lambda a: a[1]), reverse=True)
+print("Successful applicants:")
+for i in range(num_to_accept):
+    print(applicants[i][0])
