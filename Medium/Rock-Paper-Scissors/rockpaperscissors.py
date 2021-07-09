424693c83d3bda60d345077292714734fde739b4
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-08 20:39:53 -0500

    Rock-Paper_scissors Stage 5

--- Medium/Rock-Paper-Scissors/rockpaperscissors.py
+++ Medium/Rock-Paper-Scissors/rockpaperscissors.py
@@ -1,13 +1,24 @@
 import random
 
 
+def get_options():
+    wins = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+    options = input()
+    if options != "":
+        options = options.split(",")
+        for i, option in enumerate(options):
+            new_order = options[i + 1:] + options[:i]
+            wins[option] = new_order[:len(new_order) // 2]
+    return wins
+
+
 def get_input(choices):
     while True:
         choice = input()
         if choice == "!exit" or choice == "!rating" or choice in choices:
             return choice
         print("Invalid input")
-        
+
 
 rating = 0
 name = input("Enter your name: ")
@@ -18,18 +29,19 @@ with open("rating.txt", "r") as file:
         if line_data[0] == name:
             rating = int(line_data[1])
             break
-better = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+better_than = get_options()
+print("Okay, let's start")
 while True:
-    computer_choice = random.choice(("rock", "paper", "scissors"))
-    player_choice = get_input(better.keys())
+    computer_choice = random.choice(list(better_than.keys()))
+    player_choice = get_input(better_than.keys())
     if player_choice == "!exit":
         break
     elif player_choice == "!rating":
         print(f"Your rating: {rating}")
-    elif player_choice == better[computer_choice]:
+    elif player_choice in better_than[computer_choice]:
         print(f"Well done. The computer chose {computer_choice} and failed")
         rating += 100
-    elif computer_choice == better[player_choice]:
+    elif computer_choice in better_than[player_choice]:
         print(f"Sorry, but the computer chose {computer_choice}")
     else:
         print(f"There is a draw ({player_choice})")
