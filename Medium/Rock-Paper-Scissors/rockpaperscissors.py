3190ad8fc649d6e50b5ada33b08d6410756769a9
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-08 19:43:26 -0500

    Rock-Paper_scissors Stage 3

--- Medium/Rock-Paper-Scissors/rockpaperscissors.py
+++ Medium/Rock-Paper-Scissors/rockpaperscissors.py
@@ -1,11 +1,24 @@
 import random
 
+
+def get_input(choices):
+    while True:
+        user_input = input()
+        if user_input == "!exit" or user_input in choices:
+            return user_input
+        print("Invalid input")
+        
+        
 better = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-computer_choice = random.choice(("rock", "paper", "scissors"))
-player_choice = input()
-if player_choice == better[computer_choice]:
-    print(f"Well done. The computer chose {computer_choice} and failed")
-elif computer_choice == better[player_choice]:
-    print(f"Sorry, but the computer chose {computer_choice}")
-else:
-    print(f"There is a draw ({player_choice})")
+while True:
+    computer_choice = random.choice(("rock", "paper", "scissors"))
+    player_choice = get_input(better.keys())
+    if player_choice == "!exit":
+        break
+    if player_choice == better[computer_choice]:
+        print(f"Well done. The computer chose {computer_choice} and failed")
+    elif computer_choice == better[player_choice]:
+        print(f"Sorry, but the computer chose {computer_choice}")
+    else:
+        print(f"There is a draw ({player_choice})")
+print("Bye!")
