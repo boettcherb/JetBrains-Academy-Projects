332d7ae3bfc7ee4edfa1bee097a20dfbb0732ecc
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-09 20:01:01 -0500

    Markdown Editor Stage 2

--- Medium/Markdown Editor/markdowneditor.py
+++ Medium/Markdown Editor/markdowneditor.py
@@ -1,10 +1,21 @@
-print("""
-# John Lennon
-or ***John Winston Ono Lennon*** was one of *The Beatles*.
-Here are the songs he wrote I like the most:
-* Imagine
-* Norwegian Wood
-* Come Together
-* In My Life
-* ~~Hey Jude~~ (that was *McCartney*)
-""")
+formatters = ["plain", "bold", "italic", "header", "link", "inline-code", 
+              "ordered-list", "unordered-list", "new-line"]
+special = ["!done", "!help"]
+
+
+def get_input():
+    global formatters
+    while True:
+        user_input = input("Choose a formatter: ")
+        if user_input in formatters or user_input in special:
+            return user_input
+        print("Unknown formatting type or command")
+
+
+while True:
+    user_formatter = get_input()
+    if user_formatter == "!done":
+        break
+    if user_formatter == "!help":
+        print("Available formatters:", *formatters)
+        print("Special commands:", *special)
