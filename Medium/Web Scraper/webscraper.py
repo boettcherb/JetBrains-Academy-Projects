2dc384924ccfa72a0c8fdbd6a430a9525ea31b0a
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-13 16:32:05 -0500

    Web Scraper Stage 2

--- Medium/Web Scraper/webscraper.py
+++ Medium/Web Scraper/webscraper.py
@@ -1,14 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
-    r = requests.get(input("Input the URL:\n"))
-    if r.status_code == 200:
-        content = r.json().get("content", "")
-        if content:
-            print(content)
-            return
-    print("Invalid quote resource!")
+    r = requests.get(input())
+    soup = BeautifulSoup(r.content, 'html.parser')
+    head = soup.find('h1')
+    desc = soup.find('p')
+    rev = soup.find('span', text="User reviews")
+    try:
+        if "User reviews" in rev.text:
+            print({"title": head.text, "description": desc.text})
+    except AttributeError:
+        print("Invalid movie page!")
 
 
 if __name__ == "__main__":
