885eff8aa905e24f0451bc1f449d4ed87aee6ec1
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-13 16:38:43 -0500

    Web Scraper Stage 3

--- Medium/Web Scraper/webscraper.py
+++ Medium/Web Scraper/webscraper.py
@@ -1,18 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
 
 
 def main():
     r = requests.get(input())
-    soup = BeautifulSoup(r.content, 'html.parser')
-    head = soup.find('h1')
-    desc = soup.find('p')
-    rev = soup.find('span', text="User reviews")
-    try:
-        if "User reviews" in rev.text:
-            print({"title": head.text, "description": desc.text})
-    except AttributeError:
-        print("Invalid movie page!")
+    if r.status_code == 200:
+        with open("source.html", "wb") as html_file:
+            html_file.write(r.content)
+            print("Content saved.")
+    else:
+        print(f"The URL returned {r.status_code}!")
 
 
 if __name__ == "__main__":
