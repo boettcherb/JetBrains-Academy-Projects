e93c37b452ea1cfb37ce4588596d3a3c50949cb6
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-07-29 14:11:18 -0500

    To-Do List Stage 4

--- Hard/To-Do List/todolist.py
+++ Hard/To-Do List/todolist.py
@@ -25,50 +25,51 @@ def get_id(session):
     return task_id
 
 
+def print_tasks(session, message, print_dl=False, condition=True, t_order=None):
+    rows = session.query(Table).filter(condition).all() if t_order is None \
+        else session.query(Table).filter(condition).order_by(t_order).all()
+    if not rows:
+        print(message)
+    else:
+        for i, row in enumerate(rows):
+            if print_dl:
+                deadline = row.deadline.strftime('%d %b').lstrip("0")
+                print(f"{i + 1}) {row}. {deadline}")
+            else:
+                print(f"{i + 1}) {row}")
+
+
 def main():
     engine = create_engine(f"sqlite:///{DB_FILE_NAME}?check_same_thread=False")
     Base.metadata.create_all(engine)
-
     session = sessionmaker(bind=engine)()
-
     task_id = get_id(session) + 1
     while True:
-        option = input("1) Today's tasks\n2) Week's tasks\n3) All tasks\n"
-                       "4) Add task\n0) Exit\n")
+        option = input("1) Today's tasks\n2) Week's tasks\n3) All tasks\n4) "
+                       "Missed tasks\n5) Add task\n6) Delete task\n0) Exit\n")
         if option == "0":
             break
         elif option == "1":
             today = datetime.today()
             condition = Table.deadline == today.date()
-            rows = session.query(Table).filter(condition).all()
             print(f"\nToday {datetime.today().strftime('%d %b')}:")
-            if not rows:
-                print("Nothing to do!")
-            else:
-                for i, row in enumerate(rows):
-                    print(f"{i + 1}) {row}")
+            print_tasks(session, "Nothing to do!", condition=condition)
         elif option == "2":
             cur_day = datetime.today().date()
             for _ in range(7):
                 print(f"\n{cur_day.strftime('%A %d %b').replace(' 0', ' ')}")
                 condition = Table.deadline == cur_day
-                rows = session.query(Table).filter(condition).all()
-                if not rows:
-                    print("Nothing to do!")
-                else:
-                    for i, row in enumerate(rows):
-                        print(f"{i + 1}) {row}")
+                print_tasks(session, "Nothing to do!", condition=condition)
                 cur_day = cur_day + timedelta(days=1)
         elif option == "3":
-            rows = session.query(Table).order_by(Table.deadline).all()
             print("\nAll tasks:")
-            if not rows:
-                print("Nothing to do!")
-            else:
-                for i, row in enumerate(rows):
-                    deadline = row.deadline.strftime('%d %b').lstrip("0")
-                    print(f"{i + 1}) {row}. {deadline}")
+            print_tasks(session, "Nothing to do!", True, t_order=Table.deadline)
         elif option == "4":
+            print("\nMissed tasks:")
+            condition = Table.deadline < datetime.today().date()
+            print_tasks(session, "Nothing is missed!", print_dl=True,
+                        condition=condition, t_order=Table.deadline)
+        elif option == "5":
             task = input("\nEnter task\n")
             deadline = datetime.strptime(input("Enter deadline\n"), "%Y-%m-%d")
             new_row = Table(id=task_id, task=task, deadline=deadline)
@@ -76,6 +77,15 @@ def main():
             session.commit()
             task_id += 1
             print("The task has been added!")
+        elif option == "6":
+            rows = session.query(Table).order_by(Table.deadline).all()
+            if not rows:
+                print("Nothing to delete")
+            else:
+                print("\nChoose the number of the task you want to delete:")
+                print_tasks(session, None, True, t_order=Table.deadline)
+                session.delete(rows[int(input()) - 1])
+                session.commit()
         print()
     print("\nBye!")
 
