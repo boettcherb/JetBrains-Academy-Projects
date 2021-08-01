d5096889da61caf0b247ed3d3db441029aaee12b
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-08-01 14:41:24 -0500

    Smart Calculator Stage 2

--- Hard/Smart Calculator/smartcalculator.py
+++ Hard/Smart Calculator/smartcalculator.py
@@ -1,1 +1,7 @@
-print(sum(int(x) for x in input().split()))
+while True:
+    numbers = input()
+    if numbers == "/exit":
+        break
+    if numbers != "":
+        print(sum(int(x) for x in numbers.split()))
+print("Bye!")
