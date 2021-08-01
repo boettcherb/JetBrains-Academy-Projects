a59ed2daab7b4db7266a665ef7524e95cb5cefeb
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-08-01 14:44:45 -0500

    Smart Calculator Stage 3

--- Hard/Smart Calculator/smartcalculator.py
+++ Hard/Smart Calculator/smartcalculator.py
@@ -2,6 +2,8 @@ while True:
     numbers = input()
     if numbers == "/exit":
         break
-    if numbers != "":
+    elif numbers == "/help":
+        print("The program calculates the sum of numbers")
+    elif numbers != "":
         print(sum(int(x) for x in numbers.split()))
 print("Bye!")
