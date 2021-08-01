d02cfb417d65b88024e340263c795268f1d3150b
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-08-01 15:21:15 -0500

    Smart Calculator Stage 5

--- Hard/Smart Calculator/smartcalculator.py
+++ Hard/Smart Calculator/smartcalculator.py
@@ -1,15 +1,50 @@
-while True:
-    numbers = input()
-    if numbers == "/exit":
-        break
-    elif numbers == "/help":
-        print("The program calculates expressions using the + and - operators")
-    elif numbers != "":
-        expression = numbers.split()
-        result = int(expression[0])
-        for i in range(len(expression) // 2):
-            op = expression[i * 2 + 1]
-            number = int(expression[i * 2 + 2])
-            result += number if op == "+" or len(op) % 2 == 0 else -number
-        print(result)
-print("Bye!")
+def process_command(command):
+    if command == "/exit":
+        return "exit"
+    if command == "/help":
+        print("Calculates expressions using the + and - operators")
+    else:
+        print("Unknown command")
+    return "continue"
+
+
+def valid_expression(expression):
+    parts = expression.split()
+    if len(parts) % 2 == 0:
+        return False
+    for i, part in enumerate(parts):
+        if i % 2 == 0:
+            try:
+                int(part)
+            except ValueError:
+                return False
+        else:
+            if part != ("+" * len(part)) and part != ("-" * len(part)):
+                return False
+    return True
+
+
+def main():
+    while True:
+        expression = input()
+        if expression == "":
+            continue
+        elif expression[0] == "/":
+            if process_command(expression) == "exit":
+                break
+        else:
+            if not valid_expression(expression):
+                print("Invalid expression")
+                continue
+            expression = expression.split()
+            result = int(expression[0])
+            for i in range(len(expression) // 2):
+                op = expression[i * 2 + 1]
+                number = int(expression[i * 2 + 2])
+                result += number if op == "+" or len(op) % 2 == 0 else -number
+            print(result)
+    print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
