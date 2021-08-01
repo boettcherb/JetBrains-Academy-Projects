d61f5957831efefe3f5888d280f8650a8251de5a
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-08-01 14:11:13 -0500

    Memorization Tool Stage 4

--- Hard/Memorization Tool/memorizationtool.py
+++ Hard/Memorization Tool/memorizationtool.py
@@ -10,14 +10,17 @@ class Flashcard(Base):
     id = Column(Integer, primary_key=True)
     question = Column(String)
     answer = Column(String)
+    box = Column(Integer)
 
 
-def get_option(prompt, min_option, max_option):
+def get_option(prompt, options):
     while True:
         print(prompt)
         option = input()
         try:
-            if min_option <= int(option) <= max_option:
+            if option in options:
+                return option
+            if int(option) in options:
                 return int(option)
         except ValueError:
             pass
@@ -37,14 +40,15 @@ def get_flashcard(session):
         question = input("Question:\n").strip()
     while not answer:
         answer = input("Answer:\n").strip()
-    session.add(Flashcard(id=get_id(session), question=question, answer=answer))
+    session.add(Flashcard(id=get_id(session), question=question,
+                          answer=answer, box=1))
     session.commit()
 
 
 def add_flashcards(session):
     prompt = "1. Add a new flashcard\n2. Exit"
     while True:
-        option = get_option(prompt, 1, 2)
+        option = get_option(prompt, (1, 2))
         if option == 1:
             print()
             get_flashcard(session)
@@ -54,13 +58,13 @@ def add_flashcards(session):
 
 
 def update_card(session, card):
-    print('press "d" to delete the flashcard:')
-    print('press "e" to edit the flashcard:')
-    choice = input()
-    if choice == "d":
+    prompt = 'press "d" to delete the flashcard:\n' \
+             'press "e" to edit the flashcard: '
+    option = get_option(prompt, ("d", "e"))
+    if option == "d":
         session.delete(card)
         session.commit()
-    elif choice == "e":
+    else:
         print(f"current question: {card.question}")
         new_question = input("please write a new question: ").strip()
         if new_question != "":
@@ -72,6 +76,20 @@ def update_card(session, card):
         session.commit()
 
 
+def ask_if_correct(session, card):
+    prompt = 'press "y" if your answer is correct:\n' \
+             'press "n" if your answer is wrong:'
+    option = get_option(prompt, ("y", "n"))
+    if option == "y":
+        if card.box == 3:
+            session.delete(card)
+        else:
+            card.box += 1
+    else:
+        card.box = 1
+    session.commit()
+
+
 def practice_flashcards(session):
     flashcards = session.query(Flashcard).all()
     if not flashcards:
@@ -79,15 +97,15 @@ def practice_flashcards(session):
         return
     for card in flashcards:
         print(f"\nQuestion: {card.question}")
-        print('press "y" to see the answer:')
-        print('press "n" to skip:')
-        print('press "u" to update:')
-        choice = input()
-        if choice == "y":
+        prompt = 'press "y" to see the answer:\npress "n" to skip:\n' \
+                 'press "u" to update:'
+        option = get_option(prompt, ("y", "n", "u"))
+        if option == "y":
             print(f"\nAnswer: {card.answer}")
-        elif choice == "n":
-            print()
-        elif choice == "u":
+            ask_if_correct(session, card)
+        elif option == "n":
+            ask_if_correct(session, card)
+        else:
             update_card(session, card)
 
 
@@ -97,7 +115,7 @@ def main():
     session = sessionmaker(bind=engine)()
     prompt = "1. Add flashcards\n2. Practice flashcards\n3. Exit"
     while True:
-        option = get_option(prompt, 1, 3)
+        option = get_option(prompt, (1, 2, 3))
         if option == 1:
             print()
             add_flashcards(session)
