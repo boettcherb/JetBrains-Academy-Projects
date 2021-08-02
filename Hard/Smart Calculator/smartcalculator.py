44fe666c72c1cd4403dc8ecd9873867e64e7caad
Brandon Boettcher <brandonboettcher19@gmail.com>
2021-08-02 15:20:30 -0500

    Smart Calculator Stage 7

--- Hard/Smart Calculator/smartcalculator.py
+++ Hard/Smart Calculator/smartcalculator.py
@@ -1,4 +1,31 @@
 from string import ascii_letters
+from collections import deque
+
+
+class Stack:
+    def __init__(self):
+        self.stack = deque()
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def top(self):
+        top_value = self.stack.pop()
+        self.stack.append(top_value)
+        return top_value
+
+    def empty(self):
+        return len(self.stack) == 0
+
+    def __repr__(self):
+        return str(list(self.stack))
+
+    def __len__(self):
+        return len(self.stack)
+
 
 variables = {}
 
@@ -13,26 +40,50 @@ def process_command(command):
     return "continue"
 
 
-def valid_expression(expression):
-    parts = expression.split()
-    if len(parts) % 2 == 0:
-        print("Invalid expression")
-        return False
-    for i, part in enumerate(parts):
-        if i % 2 == 0 and get_value(part) is None:
-            print("Unknown variable")
-            return False
-        if i % 2 == 1 and part != ("+" * len(part)) and part != ("-" * len(part)):
-            print("Invalid expression")
-            return False
-    return True
-
-
 def get_value(operand):
     try:
         return int(operand)
-    except:
+    except ValueError:
         return variables.get(operand, None)
+
+
+def check_parentheses(expression):
+    depth = 0
+    for c in expression:
+        depth += c == "("
+        depth -= c == ")"
+        if depth < 0:
+            return False
+    return depth == 0
+
+
+def parse_expression(expression):
+    if not check_parentheses(expression):
+        return "Invalid expression"
+    while expression.count("++"):
+        expression = expression.replace("++", "+")
+    while expression.count("---"):
+        expression = expression.replace("---", "-")
+    while expression.count("--"):
+        expression = expression.replace("--", "+")
+    for op in "+-*/()":
+        expression = expression.replace(op, f" {op} ")
+    expression = expression.split()
+    operand = True
+    for i, part in enumerate(expression):
+        if part in "()":
+            continue
+        if operand:
+            value = get_value(part)
+            if value is None:
+                if any(c not in ascii_letters for c in part):
+                    return "Invalid identifier"
+                return "Unknown variable"
+            expression[i] = value
+        elif part not in "+-*/()":
+            return "Invalid expression"
+        operand = not operand
+    return expression
 
 
 def process_assignment(expression):
@@ -53,6 +104,51 @@ def process_assignment(expression):
         variables[left] = val
 
 
+def convert_to_postfix(prefix):
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
+    postfix = Stack()
+    operators = Stack()
+    for part in prefix:
+        if isinstance(part, int):
+            postfix.push(part)
+        elif part == "(":
+            operators.push(part)
+        elif part == ")":
+            while operators.top() != "(":
+                postfix.push(operators.pop())
+            operators.pop()
+        elif part in "+-*/":
+            while not operators.empty() and operators.top() != "(":
+                if precedence[part] <= precedence[operators.top()]:
+                    postfix.push(operators.pop())
+                else:
+                    break
+            operators.push(part)
+    while not operators.empty():
+        postfix.push(operators.pop())
+    return postfix
+
+
+def get_operand(postfix):
+    if isinstance(postfix.top(), int):
+        return postfix.pop()
+    return evaluate_postfix(postfix)
+
+
+def evaluate_postfix(postfix):
+    if len(postfix) == 1:
+        return postfix.pop()
+    operator = postfix.pop()
+    right, left = get_operand(postfix), get_operand(postfix)
+    if operator == "+":
+        return left + right
+    if operator == "-":
+        return left - right
+    if operator == "*":
+        return left * right
+    return left // right
+
+
 def main():
     while True:
         expression = input()
@@ -65,16 +161,11 @@ def main():
             if expression.count("="):
                 process_assignment(expression)
                 continue
-            if not valid_expression(expression):
-                print("Invalid expression")
+            prefix = parse_expression(expression)
+            if isinstance(prefix, str):
+                print(prefix)
                 continue
-            expression = expression.split()
-            result = get_value(expression[0])
-            for i in range(len(expression) // 2):
-                op = expression[i * 2 + 1]
-                number = get_value(expression[i * 2 + 2])
-                result += number if op == "+" or len(op) % 2 == 0 else -number
-            print(result)
+            print(evaluate_postfix(convert_to_postfix(prefix)))
     print("Bye!")
 
 
